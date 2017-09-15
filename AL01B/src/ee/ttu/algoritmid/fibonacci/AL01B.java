475b85af57e7dd0cceb585b69e8d289ff7d08849
jakell <jakell@ttu.ee>
2017-09-15 09:29:52 +0300

    B push

---
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -0,0 +1,41 @@
+package ee.ttu.algoritmid.fibonacci;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+public class AL01B {
+
+    /**
+     * Estimate or find the exact time required to compute the n-th Fibonacci number.
+     * @param n The n-th number to compute.
+     * @return The time estimate or exact time in YEARS.
+     */
+    public String timeToComputeRecursiveFibonacci(int n) {
+        long start = System.currentTimeMillis();
+        final int baseTimeFibonacciNumber = 30;
+        double goldenRatio = 1.61803398875;
+        recursiveF(baseTimeFibonacciNumber);
+        double end = (double)(System.currentTimeMillis() - start) / baseTimeFibonacciNumber;
+        end *= 3 * n - 2;
+        BigDecimal result = BigDecimal.valueOf(end).divide(new BigDecimal("31536000000"), BigDecimal.ROUND_HALF_EVEN);
+
+        return result.toString();
+    }
+
+    /**
+     * Compute the Fibonacci sequence number recursively.
+     * (You need this in the timeToComputeRecursiveFibonacci(int n) function!)
+     * @param n The n-th number to compute.
+     * @return The n-th Fibonacci number as a string.
+     */
+    public BigInteger recursiveF(int n) {
+        if (n <= 1)
+            return BigInteger.valueOf(n);
+        return recursiveF(n - 1).add(recursiveF(n - 2));
+    }
+
+    public static void main(String[] args) {
+        AL01B timer = new AL01B();
+        System.out.println(timer.timeToComputeRecursiveFibonacci(50));
+    }
+}
