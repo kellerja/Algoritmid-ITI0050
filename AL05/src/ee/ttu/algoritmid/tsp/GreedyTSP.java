f74b02de172376c11349676494d3273b452dc36a
jakell <jakell@ttu.ee>
2017-11-10 14:01:18 +0200

    AL05

--- AL05/src/ee/ttu/algoritmid/tsp/GreedyTSP.java
+++ AL05/src/ee/ttu/algoritmid/tsp/GreedyTSP.java
@@ -3,32 +3,49 @@ package ee.ttu.algoritmid.tsp;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GreedyTSP {
 
     /* Greedy search */
     public static int[] greedySolution(int[][] adjacencyMatrix) {
-        List<Integer> visited = new ArrayList<>();
-        int[] bestPath = new int[adjacencyMatrix.length + 1];
-        int current = 0;
+        int[] route, bestRoute = new int[]{};
+        int length, bestLength = Integer.MAX_VALUE;
         for (int i = 0; i < adjacencyMatrix.length; i++) {
-            int tempBest = -1;
-            int bestDist = Integer.MAX_VALUE;
-            visited.add(current);
-            for (int j = 0; j < adjacencyMatrix.length; j++) {
-                int distance = adjacencyMatrix[current][j];
-                if (current != j && distance < bestDist && !visited.contains(j)) {
-                    bestDist = distance;
-                    tempBest = j;
-                }
+            route = findRouteFrom(i, adjacencyMatrix);
+            length = IntStream.of(route).sum();
+            if (length < bestLength) {
+                bestLength = length;
+                bestRoute = route;
             }
-            current = tempBest;
         }
-        for (int i = 0; i < visited.size(); i++) {
-            bestPath[i] = visited.get(i);
+        return bestRoute;
+    }
+
+    private static int[] findRouteFrom(int startIdx, int[][] adjacencyMatrix) {
+        int[] visited = new int[adjacencyMatrix.length + 1];
+        Arrays.fill(visited, -1);
+        int current = startIdx;
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            visited[i] = current;
+            current = findClosestNeighbour(current, visited, adjacencyMatrix[current]);
         }
-        bestPath[adjacencyMatrix.length] = visited.get(0);
-        return bestPath;
+        visited[adjacencyMatrix.length] = visited[0];
+        return visited;
+    }
+
+    private static int findClosestNeighbour(int node, int[] visited, int[] neighbours) {
+        int bestDist = Integer.MAX_VALUE;
+        int tempBest = -1;
+        for (int j = 0; j < neighbours.length; j++) {
+            int distance = neighbours[j];
+            int finalJ = j;
+            if (node != j && distance < bestDist && IntStream.of(visited).noneMatch(i -> i == finalJ)) {
+                bestDist = distance;
+                tempBest = j;
+            }
+        }
+        return tempBest;
     }
 
     public static void main(String[] args) {
